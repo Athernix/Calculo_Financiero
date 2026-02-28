d3eeb976c7c5d1a62508b23c3223c014636b44d2
Athernix <hanichiro8@gmail.com>
2026-02-28 12:06:30 -0500

    Arquitectura en capas

---
+++ UI/Views/ui_manin.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+class MainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Calculadora Financiera")
+        self.geometry("800x600")
+
---
+++ main.py
@@ -0,0 +1,7 @@
+#importación de Interfaz
+from UI.Views.ui_manin import MainWindow
+
+# Ejecutar la aplicación
+if __name__ == "__main__":
+    app = MainWindow()
+    app.mainloop()
